f2851ceb6430b8b66b9c3863d8f1c7f255ed96ed
Ahmet <ahmet.software.ist@gmail.com>
2025-05-15 22:38:13 +0300

    Create all controller, service, repository, entity and dto

--- src/main/java/com/ecommerce/EcoomerceApplication.java
+++ src/main/java/com/ecommerce/EcoomerceApplication.java
@@ -1,13 +1,22 @@
 package com.ecommerce;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EcoomerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcoomerceApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setAmbiguityIgnored(true);
+		return modelMapper;
 	}
 
 }
---
+++ src/main/java/com/ecommerce/model/dto/CartDto.java
@@ -0,0 +1,4 @@
+package com.ecommerce.model.dto;
+
+public class CartDto {
+}
